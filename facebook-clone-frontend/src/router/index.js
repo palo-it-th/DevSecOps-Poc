90b0ae99ddce7508a8109aedc7ea88f33afeadf1
Jack Vatcharat <vrattananun@palo-it.com>
2024-12-12 17:08:43 +0700

    add vulnerable route for executing arbitrary code in router

--- facebook-clone-frontend/src/router/index.js
+++ facebook-clone-frontend/src/router/index.js
@@ -1,16 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../views/HomePage.vue';
-import UserLogin from '@/components/UserLogin.vue';
-import RegisterUser from '@/components/RegisterUser.vue';
-import CreatePost from '@/components/CreatePost.vue';
-import PostList from '@/components/PostList.vue';
+import CreatePost from "@/components/CreatePost.vue";
+import PostList from "@/components/PostList.vue";
+import RegisterUser from "@/components/RegisterUser.vue";
+import UserLogin from "@/components/UserLogin.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../views/HomePage.vue";
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/login', name: 'Login', component: UserLogin },
-  { path: '/register', name: 'Register', component: RegisterUser },
-  { path: '/create-post', name: 'CreatePost', component: CreatePost },
-  { path: '/posts', name: 'PostList', component: PostList },
+  { path: "/", name: "Home", component: HomePage },
+  { path: "/login", name: "Login", component: UserLogin },
+  { path: "/register", name: "Register", component: RegisterUser },
+  { path: "/create-post", name: "CreatePost", component: CreatePost },
+  { path: "/posts", name: "PostList", component: PostList },
+  // Vulnerable route
+  {
+    path: "/execute",
+    name: "Execute",
+    component: {
+      template: "<div>{{ result }}</div>",
+      data() {
+        return {
+          result: "",
+        };
+      },
+      created() {
+        const userInput = this.$route.query.code;
+        this.result = eval(userInput); // Dangerous: eval allows execution of arbitrary code
+      },
+    },
+  },
 ];
 
 const router = createRouter({
